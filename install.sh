0dcab0dde34fa256e2742a386ff62cc101f46903
Cédric Pineau <cedric.pineau@gmail.com>
2011-12-02 15:24:10 +0100

    Adaptation à gnome3, suppression de l'installation de clojure

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo cp clojure.lang /usr/share/gtksourceview-2.0/language-specs/clojure.lang
+sudo cp clojure.lang /usr/share/gtksourceview-3.0/language-specs/clojure.lang
 sudo cp clojure.xml /usr/share/mime/packages/clojure.xml
-cd /usr/share/
-sudo update-mime-database mime
-sudo apt-get install clojure
-
+sudo update-mime-database /usr/share/mime
 
 
