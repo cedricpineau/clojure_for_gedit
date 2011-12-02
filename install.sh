12797239953fc0761222483937072849e7023957
Cédric Pineau <cedric.pineau@gmail.com>
2011-12-02 15:33:13 +0100

    Support de Gnome2 & Gnome3

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-sudo cp clojure.lang /usr/share/gtksourceview-3.0/language-specs/clojure.lang
+gtksourceview=/usr/share/gtksourceview-3.0
+if [ ! -d $gtksourceview ]
+then
+  echo "Gnome3 not installed, using Gnome2"
+  gtksourceview=/usr/share/gtksourceview-2.0
+fi
+sudo cp clojure.lang $gtksourceview/language-specs/clojure.lang
 sudo cp clojure.xml /usr/share/mime/packages/clojure.xml
 sudo update-mime-database /usr/share/mime
 
